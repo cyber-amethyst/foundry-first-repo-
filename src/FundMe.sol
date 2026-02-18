51a83bd8340ff7dff5b1124db896251eddb6a3e5
cyber-amethyst <chukwuvictorious3@gmail.com>
2026-02-18 06:27:38 +0100

    Add NatSpec documentation to FundMe contract
    - Added comprehensive NatSpec documentation to all functions
    - Documented contract, errors, and modifiers
    - Preserved all learning comments for educational purposes
    - Updated author to @Cyber-amethyst (Vickie)

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -5,8 +5,18 @@ pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
+/**
+ * @notice Thrown when a non-owner tries to call owner-only functions
+ * @dev Custom error is more gas efficient than using strings
+ */
 error FundMe__NotOwner();
 
+/**
+ * @title A contract for crowd funding
+ * @author @Cyber-amethyst (Vickie)
+ * @notice This contract allows users to fund and the owner to withdraw funds
+ * @dev Implements Chainlink Price Feeds to convert ETH amounts to USD
+ */
 contract FundMe {
     using PriceConverter for uint256;
 
@@ -20,11 +30,21 @@ contract FundMe {
     uint256 public constant MINIMUM_USD = 5e18; //5 * 10 ** 18;
     AggregatorV3Interface private s_priceFeed; // s_ is used to represent that it is a storage variable
 
+    /**
+     * @notice Constructor that sets the contract owner and price feed
+     * @param priceFeed Address of the Chainlink price feed contract
+     * @dev The owner is set to the deployer (msg.sender)
+     */
     constructor(address priceFeed) {
         i_owner = msg.sender;
         s_priceFeed = AggregatorV3Interface(priceFeed);
     }
 
+    /**
+     * @notice Allows users to fund the contract with ETH
+     * @dev Requires at least MINIMUM_USD worth of ETH based on current price feed
+     * @dev Adds the funder to the s_funders array and updates their total funded amount
+     */
     function fund() public payable {
         require(
             msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
@@ -35,21 +55,33 @@ contract FundMe {
         s_funders.push(msg.sender);
     }
 
+    /**
+     * @notice Gets the version of the Chainlink price feed being used
+     * @return The version number of the price feed aggregator
+     */
     function getVersion() public view returns (uint256) {
         return s_priceFeed.version();
     }
 
+    /**
+     * @dev Modifier to restrict function access to only the contract owner
+     */
     modifier onlyOwner() {
         // require(msg.sender == owner);
         if (msg.sender != i_owner) revert FundMe__NotOwner();
         _;
     }
 
+    /**
+     * @notice Allows the owner to withdraw all funds (gas optimized version)
+     * @dev More gas efficient than withdraw() by caching the funders array length in memory
+     * @dev Resets all funder balances and the funders array, then transfers all ETH to owner
+     */
     function cheaperWithdraw() public onlyOwner {
         uint256 fundersLength = s_funders.length;
         for (
             uint256 funderIndex = 0;
-            funderIndex < fundersLength; //This will save gas by avoiding reading s_funders.length on every loop and instead reading it once. funderLenght is now a memory variable
+            funderIndex < fundersLength; //This will save gas by avoiding reading s_funders.length on every loop and instead read it once. funderLenght is now a memory variable
             funderIndex++
         ) {
             address funder = s_funders[funderIndex];
@@ -62,6 +94,11 @@ contract FundMe {
         require(callSuccess, "Call failed"); //there is not much to do here, its already gas efficient
     }
 
+    /**
+     * @notice Allows the owner to withdraw all funds
+     * @dev Resets all funder balances and the funders array, then transfers all ETH to owner
+     * @dev Uses the call method which is the recommended way to transfer ETH
+     */
     function withdraw() public onlyOwner {
         for (
             uint256 funderIndex = 0;
@@ -98,10 +135,18 @@ contract FundMe {
     //  /        \
     //receive()  fallback()
 
+    /**
+     * @notice Fallback function that redirects to fund()
+     * @dev Called when contract receives ETH with data that doesn't match any function
+     */
     fallback() external payable {
         fund();
     }
 
+    /**
+     * @notice Receive function that redirects to fund()
+     * @dev Called when contract receives plain ETH with no data
+     */
     receive() external payable {
         fund();
     }
@@ -109,16 +154,30 @@ contract FundMe {
     /**     Creating getter functions
      * View / Pure function will be our 'Getters'
      */
+    /**
+     * @notice Gets the total amount funded by a specific address
+     * @param fundingAddress The address to query
+     * @return The amount in wei that the address has funded
+     */
     function getAddressToAmountFunded(
         address fundingAddress
     ) external view returns (uint256) {
-        return s_addressToAmountFunded[fundingAddress];
+        return s_addressToAmountFunded[fundingAddress]; 
     }
 
+    /**
+     * @notice Gets the funder address at a specific index in the funders array
+     * @param index The index position in the funders array
+     * @return The address of the funder at that index
+     */
     function getFunder(uint256 index) external view returns (address) {
         return s_funders[index];
     }
 
+    /**
+     * @notice Gets the contract owner's address
+     * @return The address of the contract owner
+     */
     function getOwner() external view returns (address) {
         return i_owner;
     }
